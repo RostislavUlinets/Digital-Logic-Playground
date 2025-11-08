eb8e06a472522851ac1bf84fd786968d323af534
Rostyslav <poct2002@gmail.com>
2025-11-08 13:52:03 +0100

    Added Logic Gate class

--- lib/domain/entities/logic_gate.dart
+++ lib/domain/entities/logic_gate.dart
@@ -1,15 +1,21 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'gate_type.dart';
+import '../../core/constants/gate_constants.dart';
 
 part 'logic_gate.freezed.dart';
 
 /// Represents a logic gate with its properties
 @freezed
-class LogicGate with _$LogicGate {
+abstract class LogicGate with _$LogicGate {
   const factory LogicGate({
     required GateType type,
     required String name,
     required String formula,
     required String description,
   }) = _LogicGate;
+
+  /// Create a LogicGate from a GateType
+  factory LogicGate.fromType(GateType type) {
+    return GateConstants.getGate(type);
+  }
 }
--- lib/domain/entities/logic_gate.freezed.dart
+++ lib/domain/entities/logic_gate.freezed.dart
@@ -1,5 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -9,202 +9,272 @@ part of 'logic_gate.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
-);
-
 /// @nodoc
 mixin _$LogicGate {
-  GateType get type => throw _privateConstructorUsedError;
-  String get name => throw _privateConstructorUsedError;
-  String get formula => throw _privateConstructorUsedError;
-  String get description => throw _privateConstructorUsedError;
 
-  /// Create a copy of LogicGate
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  $LogicGateCopyWith<LogicGate> get copyWith =>
-      throw _privateConstructorUsedError;
+ GateType get type; String get name; String get formula; String get description;
+/// Create a copy of LogicGate
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$LogicGateCopyWith<LogicGate> get copyWith => _$LogicGateCopyWithImpl<LogicGate>(this as LogicGate, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is LogicGate&&(identical(other.type, type) || other.type == type)&&(identical(other.name, name) || other.name == name)&&(identical(other.formula, formula) || other.formula == formula)&&(identical(other.description, description) || other.description == description));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,type,name,formula,description);
+
+@override
+String toString() {
+  return 'LogicGate(type: $type, name: $name, formula: $formula, description: $description)';
+}
+
+
 }
 
 /// @nodoc
-abstract class $LogicGateCopyWith<$Res> {
-  factory $LogicGateCopyWith(LogicGate value, $Res Function(LogicGate) then) =
-      _$LogicGateCopyWithImpl<$Res, LogicGate>;
-  @useResult
-  $Res call({GateType type, String name, String formula, String description});
-}
+abstract mixin class $LogicGateCopyWith<$Res>  {
+  factory $LogicGateCopyWith(LogicGate value, $Res Function(LogicGate) _then) = _$LogicGateCopyWithImpl;
+@useResult
+$Res call({
+ GateType type, String name, String formula, String description
+});
 
+
+
+
+}
 /// @nodoc
-class _$LogicGateCopyWithImpl<$Res, $Val extends LogicGate>
+class _$LogicGateCopyWithImpl<$Res>
     implements $LogicGateCopyWith<$Res> {
-  _$LogicGateCopyWithImpl(this._value, this._then);
+  _$LogicGateCopyWithImpl(this._self, this._then);
 
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
+  final LogicGate _self;
+  final $Res Function(LogicGate) _then;
 
-  /// Create a copy of LogicGate
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? type = null,
-    Object? name = null,
-    Object? formula = null,
-    Object? description = null,
-  }) {
-    return _then(
-      _value.copyWith(
-            type: null == type
-                ? _value.type
-                : type // ignore: cast_nullable_to_non_nullable
-                      as GateType,
-            name: null == name
-                ? _value.name
-                : name // ignore: cast_nullable_to_non_nullable
-                      as String,
-            formula: null == formula
-                ? _value.formula
-                : formula // ignore: cast_nullable_to_non_nullable
-                      as String,
-            description: null == description
-                ? _value.description
-                : description // ignore: cast_nullable_to_non_nullable
-                      as String,
-          )
-          as $Val,
-    );
-  }
+/// Create a copy of LogicGate
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? type = null,Object? name = null,Object? formula = null,Object? description = null,}) {
+  return _then(_self.copyWith(
+type: null == type ? _self.type : type // ignore: cast_nullable_to_non_nullable
+as GateType,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,formula: null == formula ? _self.formula : formula // ignore: cast_nullable_to_non_nullable
+as String,description: null == description ? _self.description : description // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [LogicGate].
+extension LogicGatePatterns on LogicGate {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _LogicGate value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _LogicGate() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _LogicGate value)  $default,){
+final _that = this;
+switch (_that) {
+case _LogicGate():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _LogicGate value)?  $default,){
+final _that = this;
+switch (_that) {
+case _LogicGate() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( GateType type,  String name,  String formula,  String description)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _LogicGate() when $default != null:
+return $default(_that.type,_that.name,_that.formula,_that.description);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( GateType type,  String name,  String formula,  String description)  $default,) {final _that = this;
+switch (_that) {
+case _LogicGate():
+return $default(_that.type,_that.name,_that.formula,_that.description);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( GateType type,  String name,  String formula,  String description)?  $default,) {final _that = this;
+switch (_that) {
+case _LogicGate() when $default != null:
+return $default(_that.type,_that.name,_that.formula,_that.description);case _:
+  return null;
+
+}
+}
+
 }
 
 /// @nodoc
-abstract class _$$LogicGateImplCopyWith<$Res>
-    implements $LogicGateCopyWith<$Res> {
-  factory _$$LogicGateImplCopyWith(
-    _$LogicGateImpl value,
-    $Res Function(_$LogicGateImpl) then,
-  ) = __$$LogicGateImplCopyWithImpl<$Res>;
-  @override
-  @useResult
-  $Res call({GateType type, String name, String formula, String description});
+
+
+class _LogicGate implements LogicGate {
+  const _LogicGate({required this.type, required this.name, required this.formula, required this.description});
+  
+
+@override final  GateType type;
+@override final  String name;
+@override final  String formula;
+@override final  String description;
+
+/// Create a copy of LogicGate
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$LogicGateCopyWith<_LogicGate> get copyWith => __$LogicGateCopyWithImpl<_LogicGate>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _LogicGate&&(identical(other.type, type) || other.type == type)&&(identical(other.name, name) || other.name == name)&&(identical(other.formula, formula) || other.formula == formula)&&(identical(other.description, description) || other.description == description));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,type,name,formula,description);
+
+@override
+String toString() {
+  return 'LogicGate(type: $type, name: $name, formula: $formula, description: $description)';
+}
+
+
 }
 
 /// @nodoc
-class __$$LogicGateImplCopyWithImpl<$Res>
-    extends _$LogicGateCopyWithImpl<$Res, _$LogicGateImpl>
-    implements _$$LogicGateImplCopyWith<$Res> {
-  __$$LogicGateImplCopyWithImpl(
-    _$LogicGateImpl _value,
-    $Res Function(_$LogicGateImpl) _then,
-  ) : super(_value, _then);
+abstract mixin class _$LogicGateCopyWith<$Res> implements $LogicGateCopyWith<$Res> {
+  factory _$LogicGateCopyWith(_LogicGate value, $Res Function(_LogicGate) _then) = __$LogicGateCopyWithImpl;
+@override @useResult
+$Res call({
+ GateType type, String name, String formula, String description
+});
 
-  /// Create a copy of LogicGate
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? type = null,
-    Object? name = null,
-    Object? formula = null,
-    Object? description = null,
-  }) {
-    return _then(
-      _$LogicGateImpl(
-        type: null == type
-            ? _value.type
-            : type // ignore: cast_nullable_to_non_nullable
-                  as GateType,
-        name: null == name
-            ? _value.name
-            : name // ignore: cast_nullable_to_non_nullable
-                  as String,
-        formula: null == formula
-            ? _value.formula
-            : formula // ignore: cast_nullable_to_non_nullable
-                  as String,
-        description: null == description
-            ? _value.description
-            : description // ignore: cast_nullable_to_non_nullable
-                  as String,
-      ),
-    );
-  }
+
+
+
 }
-
 /// @nodoc
+class __$LogicGateCopyWithImpl<$Res>
+    implements _$LogicGateCopyWith<$Res> {
+  __$LogicGateCopyWithImpl(this._self, this._then);
 
-class _$LogicGateImpl implements _LogicGate {
-  const _$LogicGateImpl({
-    required this.type,
-    required this.name,
-    required this.formula,
-    required this.description,
-  });
+  final _LogicGate _self;
+  final $Res Function(_LogicGate) _then;
 
-  @override
-  final GateType type;
-  @override
-  final String name;
-  @override
-  final String formula;
-  @override
-  final String description;
-
-  @override
-  String toString() {
-    return 'LogicGate(type: $type, name: $name, formula: $formula, description: $description)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _$LogicGateImpl &&
-            (identical(other.type, type) || other.type == type) &&
-            (identical(other.name, name) || other.name == name) &&
-            (identical(other.formula, formula) || other.formula == formula) &&
-            (identical(other.description, description) ||
-                other.description == description));
-  }
-
-  @override
-  int get hashCode =>
-      Object.hash(runtimeType, type, name, formula, description);
-
-  /// Create a copy of LogicGate
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  @pragma('vm:prefer-inline')
-  _$$LogicGateImplCopyWith<_$LogicGateImpl> get copyWith =>
-      __$$LogicGateImplCopyWithImpl<_$LogicGateImpl>(this, _$identity);
+/// Create a copy of LogicGate
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? type = null,Object? name = null,Object? formula = null,Object? description = null,}) {
+  return _then(_LogicGate(
+type: null == type ? _self.type : type // ignore: cast_nullable_to_non_nullable
+as GateType,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,formula: null == formula ? _self.formula : formula // ignore: cast_nullable_to_non_nullable
+as String,description: null == description ? _self.description : description // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
 }
 
-abstract class _LogicGate implements LogicGate {
-  const factory _LogicGate({
-    required final GateType type,
-    required final String name,
-    required final String formula,
-    required final String description,
-  }) = _$LogicGateImpl;
 
-  @override
-  GateType get type;
-  @override
-  String get name;
-  @override
-  String get formula;
-  @override
-  String get description;
-
-  /// Create a copy of LogicGate
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$LogicGateImplCopyWith<_$LogicGateImpl> get copyWith =>
-      throw _privateConstructorUsedError;
 }
+
+// dart format on
