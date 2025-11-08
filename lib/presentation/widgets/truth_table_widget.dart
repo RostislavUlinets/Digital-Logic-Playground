ed6e26763de2652f4ac4b2c69ba86212cea4fd28
Rostyslav <poct2002@gmail.com>
2025-11-08 15:29:01 +0100

    Fixed problems

--- lib/presentation/widgets/truth_table_widget.dart
+++ lib/presentation/widgets/truth_table_widget.dart
@@ -108,7 +108,7 @@ class _TruthTableWidgetState extends State<TruthTableWidget> {
                         ),
                       ),
                     );
-                  }).toList(),
+                  }),
                 ],
               ),
             ),
